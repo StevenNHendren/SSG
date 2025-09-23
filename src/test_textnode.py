028a656665c5ddea9c988450c9d35401bf8d97fd
StevenNHendren <tebtengri@gmail.com>
2025-09-23 15:55:27 +0000

    text to html

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -35,6 +35,24 @@ class TestTextNode(unittest.TestCase):
         html_node = text_node_to_html_node(node)
         self.assertEqual(html_node.tag, "b")
         self.assertEqual(html_node.value, "This is a bold text node")
+    def test_code(self):
+        node = TextNode("This is a code text block", TextType.CODE)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "code")
+        self.assertEqual(html_node.value, "This is a code text block")
+    def test_link(self):
+        node = TextNode("This is a link text node", TextType.LINK, "https://www.microsoft.com")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link text node")
+        self.assertEqual(html_node.props["href"], "https://www.microsoft.com")
+    def test_image(self):
+        node = TextNode("This is image alt text", TextType.IMAGE, "https://www.foodbanjo.com/wp-content/uploads/2024/04/air-fryer-pigs-in-a-blanket-7025-01.jpg")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(html_node.props["alt"], "This is image alt text")
+        self.assertEqual(html_node.props["src"], "https://www.foodbanjo.com/wp-content/uploads/2024/04/air-fryer-pigs-in-a-blanket-7025-01.jpg")
 
 
 
